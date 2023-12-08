8bb4cd99a9554ef22aa2fcad95dbfefe89f8c150
andrewnguyen-nsw <andrewnguyen.nsw@gmail.com>
2023-12-08 21:29:42 +1100

    Fix env variables

--- app/preset/checkout/page.jsx
+++ app/preset/checkout/page.jsx
@@ -8,7 +8,7 @@ import CheckoutForm from "@components/CheckoutForm/CheckoutForm";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
-const stripePromise = loadStripe(`pk_test_51NvnnuB5LtC1NaASmv7doGyeEz7sSQV0VeBMz4HALdobXMpzyHFh9fT2lK9Z82A7GZRD8hf8i8K1FCEB6CzZeUBI00VGei0HVJ`);
+const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_KEY}`);
 
 const Checkout = () => {
   const [clientSecret, setClientSecret] = useState("");
