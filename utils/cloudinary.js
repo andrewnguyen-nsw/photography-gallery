8994e66ae3331762d4f1350469aa96ed0cbe3651
andrewnguyen-nsw <andrewnguyen.nsw@gmail.com>
2024-01-04 11:41:35 +1100

    Change in cache

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -51,6 +51,11 @@ export const fetchGalleryFolder = async () => {
         'Authorization': authString
       },
       cache: 'no-store',
+      next: { // Prevent CDN caching
+        'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+        'CDN-Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate', // Prevent CDN caching
+        'Vercel-CDN-Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate', // Specifically prevent Vercel Edge Cache
+      }
     });
 
     if (!response.ok) {
